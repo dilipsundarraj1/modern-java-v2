91920d539f8c8b9fe325bd8c65152fa0a257c12c
Dilip.Sundarraj <dilip.sundarraj@gmail.com>
2025-10-10 12:10:06 -0500

    refactor: update build.gradle.kts dependencies and add UnnamedVariables example

--- modern-java/build.gradle.kts
+++ modern-java/build.gradle.kts
@@ -20,13 +20,24 @@ subprojects {
         targetCompatibility = JavaVersion.VERSION_25
     }
 
+    dependencies {
+
+        testImplementation(platform("org.junit:junit-bom:5.9.1"))
+        testImplementation("org.junit.jupiter:junit-jupiter")
+        testImplementation("org.junit.jupiter:junit-jupiter-params")
+        testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    }
+
+
     tasks.withType<JavaCompile> {
         val compilerArgs = options.compilerArgs
-        compilerArgs.add("--enable-preview")
+//        compilerArgs.add("--enable-preview")
+
     }
 
     tasks.test {
         useJUnitPlatform()
-        jvmArgs(listOf("--enable-preview"))
+//        jvmArgs(listOf("--enable-preview"))
     }
 }
